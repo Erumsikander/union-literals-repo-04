16568da0c28afa6942ae880c2f104db89835cb87
Erum Sikander <mrsikanderghouri@gmail.com>
2024-07-20 10:57:44 +0500

    complete

---
+++ new.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let username = "Erum Sikander";
+console.log(username.toUpperCase());
+username = 2024;
+console.log(username);
+username = true;
+console.log(username);
---
+++ new.ts
@@ -0,0 +1,12 @@
+
+let username : string | number | boolean = "Erum Sikander";
+
+console.log(username.toUpperCase());
+
+username = 2024;
+
+console.log(username);
+
+username = true;
+
+console.log(username);
